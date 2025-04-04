3acfd03ab6a25096f976525e03f647b5c7a8b552
umair <saeedahmedasked@gmail.com>
2025-04-04 21:14:31 +0500

    Credentials backend completed

---
+++ app/src/main/java/com/ub/habittracker/ui/screens/dashboardScreen/home/viewModels/HomeViewModel.kt
@@ -0,0 +1,10 @@
+package com.ub.habittracker.ui.screens.dashboardScreen.home.viewModels
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class HomeViewModel: ViewModel() {
+
+
+}
