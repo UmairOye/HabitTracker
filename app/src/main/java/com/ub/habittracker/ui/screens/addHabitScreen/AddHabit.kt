18b1736f990028c0de617672c34943d02ccf322e
umair <saeedahmedasked@gmail.com>
2025-04-07 06:19:25 +0500

    add habit screen started

---
+++ app/src/main/java/com/ub/habittracker/ui/screens/addHabitScreen/AddHabit.kt
@@ -0,0 +1,10 @@
+package com.ub.habittracker.ui.screens.addHabitScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showSystemUi = true)
+@Composable
+fun AddHabit (){
+
+}
