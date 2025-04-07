69308da4e3c28e2c010dd6be1a64b05b5f0ae7ce
umair <saeedahmedasked@gmail.com>
2025-04-07 06:23:30 +0500

    add habit screen started

--- app/src/main/java/com/ub/habittracker/ui/screens/addHabitScreen/AddHabit.kt
+++ app/src/main/java/com/ub/habittracker/ui/screens/addHabitScreen/AddHabit.kt
@@ -1,10 +1,37 @@
 package com.ub.habittracker.ui.screens.addHabitScreen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.ub.habittracker.R
+import com.ub.habittracker.ui.theme.composables.BackButton
 
 @Preview(showSystemUi = true)
 @Composable
-fun AddHabit (){
+fun AddHabit() {
+
+    Scaffold { innerPadding ->
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            BackButton(
+                color = colorResource(id = R.color.blue_color),
+                heading = stringResource(R.string.sign_up)
+            ) {
+//                onBackPressed()
+            }
+        }
+
+    }
 
 }
